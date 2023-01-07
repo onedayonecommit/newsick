17b89927e4c5a73e4303f0a74636134dd0107d11
onedayonecommit <rudghks0981@gmail.com>
2023-01-08 01:49:12 +0900

    닉네임도 중복검사

--- backend/src/create-user/service/create-user.service.ts
+++ backend/src/create-user/service/create-user.service.ts
@@ -1,24 +1,21 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { users } from '@prisma/client';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 
 @Injectable()
 export class CreateUserService {
   constructor(private readonly prismaService: PrismaService) {}
+  /** 유저 가입 함수 */
   async createUser(data: CreateUserDto) {
     await this.checkUser(data.user_email);
     await this.checkWallet(data.user_wallet_address);
-    return this.prismaService.users.create({ data });
+    await this.checkName(data.user_name);
+    return await this.prismaService.users.create({ data });
   }
 
+  /** 유저 아이디 중복 검사 함수 */
   async checkUser(user_email: string) {
-    const idResult = this.prismaService.users.findUnique({
+    const idResult = await this.prismaService.users.findUnique({
       where: { user_email },
     });
     if (idResult) {
@@ -26,12 +23,23 @@ export class CreateUserService {
     }
   }
 
+  /** 유저 지갑주소 중복 검사 함수 */
   async checkWallet(user_wallet_address: string) {
-    const walletResult = this.prismaService.users.findUnique({
+    const walletResult = await this.prismaService.users.findUnique({
       where: { user_wallet_address },
     });
     if (walletResult) {
-      throw new HttpException('이미 사용중', HttpStatus.BAD_REQUEST);
+      throw new HttpException('지갑주소 이미 사용중', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  /** 유저 닉네임 중복 검사 함수 */
+  async checkName(user_name: string) {
+    const nameResult = await this.prismaService.users.findUnique({
+      where: { user_name },
+    });
+    if (nameResult) {
+      throw new HttpException('닉네임 이미 사용중', HttpStatus.BAD_REQUEST);
     }
   }
 }
