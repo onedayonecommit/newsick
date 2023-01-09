3e7ea4043d1055780c2810f91be0955e361e04d7
onedayonecommit <rudghks0981@gmail.com>
2023-01-09 22:24:29 +0900

    feat: add swagger api docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  /** swager 옵션 */
+  const config = new DocumentBuilder()
+    .setTitle('Swagger Example')
+    .setDescription('Swagger study API description')
+    .setVersion('1.0.0')
+    .addTag('swagger')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  /** 첫번째 파라미터는 경로 localhost:${port}/api 로 들어가면 swagger 문서 볼수 있음 */
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000, () => {
     console.log('3000 server on');
   });
