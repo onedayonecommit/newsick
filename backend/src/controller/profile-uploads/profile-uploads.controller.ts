33cd21f0eef8ac3938a19a22e5d967e37dd23ab7
onedayonecommit <rudghks0981@gmail.com>
2023-01-09 22:00:14 +0900

    feat: upload profile image to aws s3

--- backend/src/controller/profile-uploads/profile-uploads.controller.ts
+++ backend/src/controller/profile-uploads/profile-uploads.controller.ts
@@ -1,14 +1,24 @@
 import {
   Controller,
+  Get,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ProfileUploadsService } from 'src/service/profile-uploads/profile-uploads.service';
 
 @Controller('profile/uploads')
 export class ProfileUploadsController {
+  constructor(private readonly profileUploadService: ProfileUploadsService) {}
+
+  @Post('hi')
+  hi(): string {
+    return 'hi';
+  }
   @Post()
-  @UseInterceptors(FileInterceptor('file')) // docs에서는 쓰라는데 왜 써야되는지 찾는중
-  uploadFile(@UploadedFile() file: Express.Multer.File) {}
+  @UseInterceptors(FileInterceptor('profile_test')) // docs에서는 쓰라는데 왜 써야되는지 찾는중
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.profileUploadService.uploadFile(file);
+  }
 }
--- backend/src/service/profile-uploads/profile-uploads.service.ts
+++ backend/src/service/profile-uploads/profile-uploads.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as AWS from 'aws-sdk';
+import { extname } from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class ProfileUploadsService {}
+export class ProfileUploadsService {
+  constructor(private readonly configService: ConfigService) {}
+  s3 = new AWS.S3({
+    region: this.configService.get<string>('AWS_REGION'),
+    credentials: {
+      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
+      secretAccessKey: this.configService.get<string>('AWS_SECRET_KEY'),
+    },
+  });
+  async uploadFile(file: Express.Multer.File) {
+    const filebasename = `${uuidv4()}${extname(file.originalname)}`;
+    console.log(`최종 파일 네임 : ${filebasename}`);
+    const AWS_S3_BUCKET = this.configService.get<string>('AWS_BUCKET_NAME');
+    const params = {
+      Bucket: AWS_S3_BUCKET,
+      Key: String(filebasename),
+      Body: file.buffer,
+    };
+    try {
+      const response = await this.s3.upload(params).promise();
+      console.log(response);
+      return response;
+    } catch (e) {
+      console.log(e);
+      throw new Error('Failed to upload file.');
+    }
+  }
+}
