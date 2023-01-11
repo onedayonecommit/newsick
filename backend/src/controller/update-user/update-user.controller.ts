a9f5504fe40d46031dbee7670f453e9d58a8eff8
onedayonecommit <rudghks0981@gmail.com>
2023-01-11 11:06:36 +0900

    feat: Add dto to use in swagger document

--- backend/src/controller/update-user/update-user.controller.ts
+++ backend/src/controller/update-user/update-user.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Patch } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { updateUserResponseDto } from 'src/dto/swagger/swagger-update-response.dto.ts/update-user-response';
 import { updateUserDto } from 'src/dto/update-user/update-user.dto';
 import { UpdateUserService } from 'src/service/update-user/update-user.service';
 
@@ -7,6 +9,11 @@ export class UpdateUserController {
   constructor(private readonly updateUserService: UpdateUserService) {}
 
   @Patch('info')
+  @ApiOperation({
+    description: `고유한 지갑 주소 기반으로 유저 정보 변경 지갑 주소는 필수로 받아야하는 사항 나머지는 전부 선택 사항`,
+    summary: 'user-info-update-api',
+  })
+  @ApiCreatedResponse({ type: updateUserResponseDto })
   async userInfoUpdate(@Body() updateUserDto: updateUserDto): Promise<object> {
     return await this.updateUserService.userInfoUpdate(updateUserDto);
   }
--- backend/src/dto/update-user/update-user.dto.ts
+++ backend/src/dto/update-user/update-user.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { CreateUserDto } from '../create-user/create-user.dto';
 
-export class updateUserDto extends PartialType(CreateUserDto) {}
+export class updateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    description: 'Requirements',
+    example: 'f12',
+  })
+  user_wallet_address: string;
+
+  @ApiProperty({
+    description: 'user want to change info anything',
+    example: 'test123456@naver.com',
+  })
+  user_email?: string;
+}
