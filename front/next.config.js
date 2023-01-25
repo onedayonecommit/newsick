d49f08d3856b686dfc69ae276e13b610727507ca
jinnyl22 <smile2374@gmail.com>
2023-01-25 12:56:38 +0900

    FEAT:sign up

--- front/next.config.js
+++ front/next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["newsic-userprofile-nft-metadata-bucket.s3.ap-northeast-2.amazonaws.com"],
-  },
-  compiler: {
-    styledComponents: true,
   },
 };
 
