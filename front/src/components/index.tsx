1ce90cf9cd406235a0c884fc2a13ba599634d0bc
jinnyl22 <smile2374@gmail.com>
2023-02-01 15:03:13 +0900

    delete:front_TS

--- front/src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-import SideBar from "./SideBar";
-import SearchBar from "./SearchBar";
-import UserBar from "./UserBar";
-import PageNationFrame from "./PageNationFrame";
-import SwitchToggle from "./SwitchToggle";
-
-export { SideBar, SearchBar, UserBar, PageNationFrame, SwitchToggle };
--- front/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-import ConnectWallet from "../components/ConnectWallet";
-
-const Home = () => {
-  return <>Main</>;
-};
-
-export default Home;
