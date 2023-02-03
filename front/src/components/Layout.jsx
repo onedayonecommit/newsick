28476c8c8e5349b062270fc2c4c1afc30adf3379
ceo7913 <ceo7913@naver.com>
2023-02-03 21:28:39 +0900

    20230203

--- front/src/components/Layout.jsx
+++ front/src/components/Layout.jsx
@@ -1,9 +1,36 @@
-import { useRef, useState } from "react";
-import { MusicPlayer, PlayBar, SearchBar, SideBar, UserBar, VolumeBox } from "../components";
-
+import { useEffect, useRef, useState } from "react";
+import { CoverPage, MusicPlayer, PlayBar, SearchBar, SideBar, UserBar, VolumeBox } from "../components";
+import { AnimatePresence, motion } from "framer-motion";
+const scrollVariant = {
+  initial:{
+    opacity:1
+  },
+  animate:{
+    opacity:0
+  },
+  transition:{
+    duration:1
+  }
+}
+const mainOpenVariant ={
+  initial:{
+    opacity:0
+  },
+  animate:{
+    opacity:1
+  },
+  transition:{
+    duration:1
+  }
+}
 const Layout = (props) => {
   const [isSoundClick, setIsSoundClick]=useState(false);
   const [isPlayerClick,setIsPlayerClick]=useState(false);
+  const [isHideCover,setIsHideCover] = useState(false);
+  
+  useEffect(() => {
+    localStorage.setItem(true, JSON.stringify(isHideCover));
+  }, [isHideCover]);
 
   const layOutRef=useRef(null)
 
@@ -13,21 +40,37 @@ const Layout = (props) => {
   const playerClick = () =>{
     setIsPlayerClick(!isPlayerClick)
   }
-
+  const handleScroll = () => {
+    setIsHideCover(!isHideCover);
+    };
+    
   return (
-    <div className="layoutFrame"  ref={layOutRef}>
-      { isPlayerClick ? <MusicPlayer layOutRef={layOutRef}/> : null}
-      {isSoundClick ? <VolumeBox/> : null}
-      <div className="layoutBox">
-        <SideBar />
-        <PlayBar soundClick={soundClick} playerClick={playerClick} />
-        <div className="contentSection">
-          <SearchBar />
-          <div className="content">{props.children}</div>
+    <motion.div>
+      <AnimatePresence>
+          <motion.div
+            variants={scrollVariant}
+            initial={!isHideCover?"initial":""}
+            animate={!isHideCover?"animate":""}
+            transition={!isHideCover?"transition":""}
+            onAnimationComplete={() => setIsHideCover(true)}
+          >
+            <CoverPage setIsHideCover={setIsHideCover}  handleScroll={handleScroll}/>
+          </motion.div>
+      </AnimatePresence>
+      <div className="layoutFrame"  ref={layOutRef}>
+        { isPlayerClick ? <MusicPlayer layOutRef={layOutRef}/> : null}
+        {isSoundClick ? <VolumeBox/> : null}
+        <div className="layoutBox">
+          <SideBar />
+          <PlayBar soundClick={soundClick} playerClick={playerClick} />
+          <div className="contentSection">
+            <SearchBar />
+            <div className="content">{props.children}</div>
+          </div>
+          <UserBar />
         </div>
-        <UserBar />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- front/src/components/index.jsx
+++ front/src/components/index.jsx
@@ -5,4 +5,5 @@ import PlayBar from "./PlayBar";
 import PageNationFrame from "./PageNationFrame";
 import VolumeBox from "./VolumeBox";
 import MusicPlayer from "./musiccomponent/MusicPlayer";
-export { SideBar, SearchBar, UserBar, PageNationFrame, PlayBar,VolumeBox,MusicPlayer };
+import CoverPage from "./CoverPage";
+export { SideBar, SearchBar, UserBar, PageNationFrame, PlayBar,VolumeBox,MusicPlayer,CoverPage };
