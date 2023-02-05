7c42b3ab644b9f330e4286bc558da129e8abacd3
[LimJunWoo] <[ceo7913@naver.com]>
2023-02-05 18:31:47 +0900

    20230205

--- front/src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-import SideBar from "./SideBar";
-import SearchBar from "./SearchBar";
-import UserBar from "./UserBar";
-import PlayBar from "./PlayBar";
-import PageNationFrame from "./PageNationFrame";
-import VolumeBox from "./VolumeBox";
-import MusicPlayer from "./musiccomponent/MusicPlayer";
-import CoverPage from "./CoverPage";
-export { SideBar, SearchBar, UserBar, PageNationFrame, PlayBar,VolumeBox,MusicPlayer,CoverPage };
