00a6ae4a1bd91b3482e1fe60a369268397cdb238
huna91 <gmyhy0510@naver.com>
2023-02-09 15:07:55 +0900

    dd

--- backend/src/music/regist-funding-music/funding-music.dto.ts
+++ backend/src/music/regist-funding-music/funding-music.dto.ts
@@ -1,3 +1,26 @@
-export class fundingMusicDto {}
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
 
-// 회원가입을 해
+export class fundingMusicDto {
+  @IsNumber()
+  funding_id: number;
+  @IsString()
+  music_name: string;
+  @IsString()
+  @IsOptional()
+  music_lyrics?: string;
+  @IsString()
+  music_genre: string;
+  @IsString()
+  @IsOptional()
+  music_maker?: string;
+  @IsString()
+  @IsOptional()
+  lyrics_maker?: string;
+  @IsString()
+  @IsOptional()
+  singer?: string;
+  @IsString()
+  album_name: string;
+  @IsBoolean()
+  title: boolean;
+}
