4e9f30d7d5593740eb5b9f5f754aaf3a5393f355
ceo7913 <ceo7913@naver.com>
2023-02-14 19:24:17 +0900

    20230214

--- front/src/components/SideBar.jsx
+++ front/src/components/SideBar.jsx
@@ -28,8 +28,12 @@ const SideBar = () => {
               <motion.div
                 whileHover={{
                   y: -1.5,
+                  boxShadow: "0px 1.6733156776526186px 1.6733156776526186px 0px rgba(0, 0, 0, 0), 0px 3.984206390969505px 3.984206390969505px 0px rgba(0, 0, 0, 0.00996), 0px 7.259321882820145px 7.259321882820145px 0px rgba(0, 0, 0, 0.01815), 0px 12.068385491961365px 12.068385491961365px 0px rgba(0, 0, 0, 0.03017), 0px 19.50049274209897px 19.50049274209897px 0px rgba(0, 0, 0, 0.04875), 0px 31.901418213351462px 31.901418213351462px 0px rgba(0, 0, 0, 0.07975), 0px 54.96035212570371px 54.96035212570371px 0px rgba(0, 0, 0, 0.1374), 0px 100px 100px 0px rgba(0, 0, 0, 0.25)",
+                  backgroundColor: "rgba(0, 0, 0, 0.2)",
+                  borderRadius: "25px",
+                  border: "1px solid rgba(255, 255, 255, 0.1)"
                 }}
-                transition={{ ease: [0.44, 0, 0.56, 1], stiffness: 500, damping: 60, mass: 1, duration: 0.3 }}
+                transition={{ ease:[0.44,0,0.56,1], stiffness: 500, damping: 60 , mass:1, duration:0.3}}
                 whileTap={{ scale: 0.9 }}
                 className="menuItem"
                 onClick={()=>{movePage(value[1])}}
--- front/src/pages/my_page/creator/index.jsx
+++ front/src/pages/my_page/creator/index.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import Graph from '../creator/Graph'
 import {motion,AnimatePresence} from "framer-motion"
 import { useState } from 'react'
 import Modal from './Modal'
 
-const dateFunndingData=[
-    [0,10],
-    [5,50],
-    [15,75],
-    [55,100],
-    [75,10],
-    [100,5],
-    [120,50],
-    [140,100],
-    [180,50],
-]
 
 
 const FunddingDateItem = [
@@ -75,69 +63,86 @@ const FunddingDateItem = [
         quantity:"수량 완료 분수 ( 5/10 )",
     },
 ]
-const MyPageCreater = () => {
+const MyPageCreator = () => {
     const [modalOpen,setModalOpen] = useState(false);
     const close = () => setModalOpen(false);
     const open = () => setModalOpen(true);
+    const block = Array.from({length:5},()=>(<div></div>))
   return (
     <div className='MyPageCreatorFrame'>
-        <AnimatePresence>
-           {modalOpen && <Modal modalOpen={modalOpen} handleClose={close}/>}
-        </AnimatePresence>
-        <div className='topSection'>
-            <div className='totalMoneySection'>
-                <div className='infoText'>
-                    <div>지금까지 펀딩된 금액 확인</div>
-                    <div/>
-                </div>
-                <div className='infoNumber'>
-                    <div>ETH</div>
-                    <div>2.255</div>
-                </div>            </div>
-            <div className='historyGrapSection'>
-                <div className='historyText'>Fundding History</div>
-                <div className='historyGrapFrame'>
-                    <Graph  data={dateFunndingData} />
-                </div>
+    <AnimatePresence>
+       {modalOpen && <Modal modalOpen={modalOpen} handleClose={close}/>}
+    </AnimatePresence>
+    <div className='topSection'>
+        <div className='totalMoneySection'>
+            <div className='infoText'>
+                <div>지금까지 펀딩된 금액 확인</div>
+                <div/>
             </div>
-        </div>
-        <div className='bottomSection'>
-            <div className='creatorHandlerFrame'>
-                <div className='handleNavBar'>
-                    <div className='text'>진행중인 펀딩 핸들러</div>
-                    <div className='button'>공지사항 확인하기</div>
-                </div>
-                <div className='funddingList'>
+            <div className='infoNumber'>
+                <div>ETH</div>
+                <div>2.255</div>
+            </div>            </div>
+        <div className='creatorNoticeSection'>
+            <div className='noticeNav'>
+                <div className='noticeText'>공지사항 확인하기</div>
+                <div className='noticeShowAll'>Show All</div>
+            </div>
+            <div className='noticeList'>
                 {
-                    FunddingDateItem.map((item)=>(
-                        <div className='funddingItemBox'>
-                            <div className='infoSection'>
-                                <div className='funddingTitle'>{item.funddingTitle}</div>
-                                <div className='funddingDate'>{item.funddingDate}</div>
-                                <div className='unitPrice'>{item.unitPrice}</div>
-                                <div className='leftDay'>{item.leftDay}</div>
-                                <div className='quantityPercentage'>{item.quantity}</div>
-                            </div>
-                            <div className='musicInputButton'>
-                                <motion.div className='buttonLine' 
-                                onClick={()=>(modalOpen ? close():open())} 
-                                whileHover={{scale:1.1}} 
-                                whileTap={{scale:0.9}}
-                                >
-                                    음원등록
-                                </motion.div>
-                            </div>
-                            <div className='funddingTerminationButton'>
-                                펀딩종료
-                            </div>
+                    block.map(()=>(
+                    <motion.div className='noticeWrap'
+                    whileHover={{scale:1.01}}
+                    >
+                        <div className='date'>2023-02-13</div>
+                        <div className='title'>Fundding Title</div>
+                        <div className='infoFrame'>
+                            <div>공지내용</div>
+                            <div>조회수</div>
                         </div>
+                    </motion.div>
                     ))
                 }
-                </div>
             </div>
         </div>
     </div>
+    <div className='bottomSection'>
+        <div className='creatorHandlerFrame'>
+            <div className='handleNavBar'>
+                <div className='text'>진행중인 펀딩 핸들러</div>
+                <div className='button'>펀 딩 등 록</div>
+            </div>
+            <div className='funddingList'>
+            {
+                FunddingDateItem.map((item)=>(
+                    <div className='funddingItemBox'>
+                        <div className='infoSection'>
+                            <div className='funddingTitle'>{item.funddingTitle}</div>
+                            <div className='funddingDate'>{item.funddingDate}</div>
+                            <div className='unitPrice'>{item.unitPrice}</div>
+                            <div className='leftDay'>{item.leftDay}</div>
+                            <div className='quantityPercentage'>{item.quantity}</div>
+                        </div>
+                        <div className='musicInputButton'>
+                            <motion.div className='buttonLine' 
+                            onClick={()=>(modalOpen ? close():open())} 
+                            whileHover={{scale:1.1}} 
+                            whileTap={{scale:0.9}}
+                            >
+                                음원등록
+                            </motion.div>
+                        </div>
+                        <div className='funddingTerminationButton'>
+                            펀딩종료
+                        </div>
+                    </div>
+                ))
+            }
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
 
-export default MyPageCreater
+export default MyPageCreator
