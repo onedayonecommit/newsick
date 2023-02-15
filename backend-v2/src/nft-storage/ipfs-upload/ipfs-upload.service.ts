c7286bbe9c872cf341650b3889c31a472e5062da
onedayonecommit <rudghks0981@gmail.com>
2023-02-15 23:56:05 +0900

    return value change

--- backend-v2/src/nft-storage/ipfs-upload/ipfs-upload.service.ts
+++ backend-v2/src/nft-storage/ipfs-upload/ipfs-upload.service.ts
@@ -12,23 +12,33 @@ export class IpfsUploadService {
     dto: metadataDto,
   ): Promise<ipfsReturnDto> {
     const fileCid = await this.client.storeBlob(new Blob([file.buffer]));
-    const fileUrl = 'https://' + fileCid + '.ipfs.nftstorage.link';
+    const fileUrl = 'https://nftstorage.link/ipfs/' + fileCid + '/';
 
     const obj = {
       name: dto.nft_name,
+      description: dto.description,
       image: fileUrl,
-      url: 'newsic.com',
-      metadata: {
+      url: 'n2wsic.com',
+      properties: {
         producer: dto.producer,
-        description: dto.description,
       },
     };
+    // {
+    //   "name": "My Awesome ERC-1155 Token",
+    //   "description": "This is an example ERC-1155 token with metadata",
+    //   "image": "https://example.com/token-image.png",
+    //   "url": "https://example.com/token-details",
+    //   "properties": {
+    //     "foo": "bar",
+    //     "baz": 42
+    //   }
+    // }
 
     const metadata = new Blob([JSON.stringify(obj)], {
       type: 'application/json',
     });
     const metadataCid = await this.client.storeBlob(metadata);
-    const metadataUrl = 'https://' + metadataCid + '.ipfs.nftstorage.link';
+    const metadataUrl = 'https://nftstorage.link/ipfs/' + metadataCid + '/';
     return { fileUrl, metadataUrl };
   }
 }
