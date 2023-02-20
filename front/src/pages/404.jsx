00fccaa902ce7eea07d399ee0d3313462ab988c7
[LimJunWoo] <[ceo7913@naver.com]>
2023-02-21 03:08:45 +0900

    20230221

--- front/src/pages/404.jsx
+++ front/src/pages/404.jsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+
 const Custom404 = () => {
-  return <h1>404 - Page Not Found</h1>;
+
+  return (
+    <div className="page404container">
+      <div className="infoFrame">
+        <div className="leftSideText">
+          <div>We are Sorry</div>
+          <div>We lost this Page</div>
+        </div>
+        <div className="rightSideButton">
+          <div className="homeButton">Home</div>
+          <div className="backButton">Back</div>
+        </div>
+      </div>
+      <div className="lostPageImg"/>
+    </div>
+  )
 };
 
 export default Custom404;
