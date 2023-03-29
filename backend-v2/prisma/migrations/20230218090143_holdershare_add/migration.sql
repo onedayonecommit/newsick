73a25816eb552b8153e0d25782aa3789fae9f0a5
onedayonecommit <rudghks0981@gmail.com>
2023-03-30 01:15:49 +0900

    .

--- backend-v2/prisma/migrations/20230218090143_holdershare_add/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `holder_share` to the `funding` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `funding` ADD COLUMN `holder_share` INTEGER NOT NULL;
--- backend-v2/prisma/migrations/20230220022630_tickeyt/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ticket_type` on the `ticket` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `ticket` DROP COLUMN `ticket_type`;
